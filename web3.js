390e3183c43161f68ca1c571b9ac0a297205cdac
25nyak <56729621+25nyak@users.noreply.github.com>
2024-08-27 16:56:42 -0500

    Update web3.js

--- web3.js
+++ web3.js
@@ -2,11 +2,11 @@
 
 // Contract addresses
 const vaultAddress = "0x4Be0933bB8d6C915e87E7B0dec723a20Bc2a119e";
-const usdcAddress = "0x..."; // Replace with the actual USDC contract address on the Polygon network
+const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // Replace with the actual USDC contract address on the Polygon network
 
 // Contract ABIs (Ensure you have both Vault and USDC ABIs)
 const vaultABI = [ /* Vault ABI here */ ];
-const usdcABI = [ /* USDC ABI here */ ];
+const usdcABI = [ [{"inputs":[{"internalType":"address","name":"_proxyTo","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_new","type":"address"},{"indexed":false,"internalType":"address","name":"_old","type":"address"}],"name":"ProxyOwnerUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_new","type":"address"},{"indexed":true,"internalType":"address","name":"_old","type":"address"}],"name":"ProxyUpdated","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"IMPLEMENTATION_SLOT","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OWNER_SLOT","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proxyOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proxyType","outputs":[{"internalType":"uint256","name":"proxyTypeId","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferProxyOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newProxyTo","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"updateAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_newProxyTo","type":"address"}],"name":"updateImplementation","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}] ];
 
 // Web3 setup
 let web3;
